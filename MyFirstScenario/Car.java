300902394a3df2f555fe97a6019a962028c9561d
jimboweb <jimboweb@users.noreply.github.com>
2018-02-04 07:31:13 -0500

    lessons 0-3

---
+++ MyFirstScenario/Car.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Car here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Car extends Actor
+{
+    /**
+     * Act - do whatever the Car wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        move(5);
+        if(Greenfoot.isKeyDown("left")){
+            turn(-10);
+        }
+        if(Greenfoot.isKeyDown("right")){
+            turn(10);
+        }
+        
+        removeTouching(Pizza.class);
+    }    
+    
+}
